b6dce1b864d18824c98b24ea892657c427124e90
Moin <moin@commlinkinfotech.com>
2021-05-24 02:04:05 +0600

    commit latest

--- src/app/UserApplication/PersonalInformation.js
+++ src/app/UserApplication/PersonalInformation.js
@@ -7,6 +7,7 @@ import PopUp from "../components/PopUp";
 import Button from "react-bootstrap/Button";
 import DocumentUploader from "../components/DocumentUploader";
 import camera from "../user-pages/camera.js";
+import { listofFirst } from "../components/extra.js";
 const userImg1 = require("../../assets/images/dummy-img.jpg");
 
 class CustomTextBox extends React.Component {
@@ -16,7 +17,7 @@ class CustomTextBox extends React.Component {
   };
   render() {
     return (
-      <div className="col-md-6 d-inline-block">
+      <div className={`col-md-${this.props.dim} d-inline-block`}>
         <div className="form-group">
           <label htmlFor="fatherName">
             {this.props.title}{" "}
@@ -219,14 +220,21 @@ export class PersonalInformation extends Component {
                       </button>
                     </div>
                     <div className="col-md-8">
-                      <CustomTextBox
-                        id="identifierNumber"
-                        title="Identifier Number"
-                        isMandatory
-                        placeholder="Enter Identifier Number"
-                        disable
-                      />
-                      <CustomTextBox
+                      {listofFirst.map((v, k) => {
+                        console.log(v, k);
+                        return (
+                          <CustomTextBox
+                            dim={v.dim}
+                            id={v.id}
+                            title={v.title}
+                            isMandatory={v.isMandatory}
+                            placeholder={v.placeholder}
+                            disable={v.disable}
+                          />
+                        );
+                      })}
+                      {/* <CustomTextBox
+                        dim={"6"}
                         id="fullNameEn"
                         title="fullName"
                         isMandatory
@@ -234,6 +242,7 @@ export class PersonalInformation extends Component {
                         disable
                       />
                       <CustomTextBox
+                        dim={"6"}
                         id="dob"
                         title="Date of birth"
                         isMandatory
@@ -241,6 +250,7 @@ export class PersonalInformation extends Component {
                         disable
                       />
                       <CustomTextBox
+                        dim={"6"}
                         id="fullNameBn"
                         title="পূর্ণনাম"
                         isMandatory
@@ -248,6 +258,7 @@ export class PersonalInformation extends Component {
                         disable
                       />
                       <CustomTextBox
+                        dim={"6"}
                         id="mobile"
                         title="Mobile Number"
                         isMandatory
@@ -255,309 +266,17 @@ export class PersonalInformation extends Component {
                         disable
                       />
                       <CustomTextBox
+                        dim={"6"}
                         id="email"
                         title="Email"
                         placeholder="Enter E-mail"
-                        disable
-                      />
+                      /> */}
                     </div>
                   </div>
                   <div className="form-header">
                     <h3 className="box-title">Personal Info</h3>
                   </div>
 
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="motherName">
-                        Mothers Name <span style={{ color: "red" }}>*</span>
-                      </label>
-
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="motherName"
-                        placeholder="Enter Mothers Name"
-                        onChange={this.ChangeHandler("motherName")}
-                        // defaultValue={values.motherName}
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="spouseName">Spouse Name</label>
-
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="spouseName"
-                        placeholder="Enter Spouse Name"
-                        // onChange={this.ChangeHandler("spouseName")}
-                        // defaultValue={values.spouseName}
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="spouseName">
-                        Nationality <span style={{ color: "red" }}>*</span>
-                      </label>
-
-                      <select
-                        className="form-control"
-                        disabled
-                        // onChange={this.ChangeHandler("nationality")}
-                        // defaultValue={values.nationality}
-                      >
-                        <option id="nationality" value="bangladeshi">
-                          BANGLADESHI
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="birthPlace">Birth Place</label>
-
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="birthPlace"
-                        placeholder="Enter Birth Place"
-                        // onChange={this.ChangeHandler("birthPlace")}
-                        // defaultValue={values.birthPlace}
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="gender">
-                        Gender <span style={{ color: "red" }}>*</span>
-                      </label>
-
-                      <select
-                        className="form-control"
-                        disabled
-                        // onChange={this.ChangeHandler("gender")}
-                        // defaultValue={values.gender}
-                      >
-                        <option id="male" value="male">
-                          Male
-                        </option>
-
-                        <option id="female" value="female">
-                          Female
-                        </option>
-
-                        <option id="third" value="third">
-                          Third
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  {/* <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="preferredProduct">
-                        Preferred Product{" "}
-                        <span style={{ color: "red" }}>*</span>
-                      </label>
-
-                      <select
-                        className="form-control"
-                        disabled
-                        // onChange={this.ChangeHandler("preferredProduct")}
-                        // defaultValue={values.preferredProduct}
-                      >
-                        <option id="saccount" value="saccount">
-                          Saving Account
-                        </option>
-
-                        <option id="caccount" value="caccount">
-                          Current Account
-                        </option>
-
-                        <option id="fdraccount" value="fdraccount">
-                          FDR Account
-                        </option>
-                      </select>
-                    </div>
-                  </div> */}
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="districtId">Presently Resided City</label>
-
-                      <select
-                        className="form-control"
-                        // onChange={this.ChangeHandler("districtId")}
-                        // defaultValue={values.districtId}
-                      >
-                        <option id="saccount" value="saccount">
-                          Resident
-                        </option>
-
-                        <option id="ctg" value="ctg">
-                          Chattogram City Corporation
-                        </option>
-
-                        <option id="dhk" value="dhk">
-                          Dhaka City Corporation
-                        </option>
-
-                        <option id="khl" value="khl">
-                          Khulna City Corporation
-                        </option>
-
-                        <option id="nar" value="nar">
-                          Narayangonj City Corporation
-                        </option>
-
-                        <option id="raj" value="raj">
-                          Rajshahi City Corporation
-                        </option>
-
-                        <option id="syl" value="syl">
-                          Sylhet City Corporation
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="branchId">Preferred Branch</label>
-
-                      <select
-                        className="form-control"
-                        // onChange={this.ChangeHandler("branchId")}
-                        // defaultValue={values.branchId}
-                      >
-                        <option id="gulshan" value="gulshan">
-                          Branch A
-                        </option>
-                        <option id="gulshan" value="gulshan">
-                          Branch B
-                        </option>
-                        <option id="gulshan" value="gulshan">
-                          Branch C
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="residentStatus">Resident Status</label>
-
-                      <select
-                        className="form-control"
-                        // onChange={this.ChangeHandler("residentStatus")}
-                        // defaultValue={values.residentStatus}
-                      >
-                        <option id="resident" value="resident">
-                          Resident
-                        </option>
-
-                        <option id="nonresident" value="nonresident">
-                          Non-Resident
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="residentialStatus">
-                        Residential Status
-                      </label>
-
-                      <select
-                        className="form-control"
-                        // onChange={this.ChangeHandler("residentialStatus")}
-                        // defaultValue={values.residentialStatus}
-                      >
-                        <option id="none" value="">
-                          select
-                        </option>
-                        <option id="own" value="own">
-                          Own
-                        </option>
-                        <option id="rented" value="rented">
-                          Rented
-                        </option>
-                        <option id="others" value="others">
-                          Others
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="occupation">Occupation</label>
-
-                      <select
-                        className="form-control"
-                        // onChange={this.ChangeHandler("occupation")}
-                        // defaultValue={values.occupation}
-                      >
-                        <option value="">select</option>
-                        <option value="service">Service</option>
-                        <option value="business">Business</option>
-                        <option value="others">Others</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-4 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="maritalStatus">
-                        Marital Status <span style={{ color: "red" }}>*</span>
-                      </label>
-
-                      <select
-                        className="form-control"
-                        // onChange={this.ChangeHandler("maritalStatus")}
-                        // defaultValue={values.maritalStatus}
-                      >
-                        <option id="none" value="">
-                          select
-                        </option>
-                        <option id="married" value="married">
-                          Married
-                        </option>
-                        <option id="singale" value="singale">
-                          Singale
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-6 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="presentAddress">
-                        Present Address <span style={{ color: "red" }}>*</span>
-                      </label>
-                      <textarea
-                        type="text"
-                        rows="3"
-                        id="permanentAddress"
-                        className="form-control"
-                        // onChange={this.ChangeHandler("presentAddress")}
-                        // defaultValue={values.presentAddress}
-                        placeholder="Enter Present Address"
-                      ></textarea>
-                    </div>
-                  </div>
-                  <div className="col-md-6 d-inline-block">
-                    <div className="form-group">
-                      <label htmlFor="permanentAddress">
-                        Permanent Address{" "}
-                        <span style={{ color: "red" }}>*</span>
-                      </label>
-                      <textarea
-                        type="text"
-                        rows="3"
-                        id="permanentAddress"
-                        className="form-control"
-                        // onChange={this.ChangeHandler("permanentAddress")}
-                        // defaultValue={values.permanentAddress}
-                        placeholder="Enter Permanent Address"
-                      ></textarea>
-                    </div>
-                  </div>
                   <div className="row justify-content-md-center">
                     <div className="col-md-12">
                       <div className="form-header">
---
+++ src/app/components/extra.js
@@ -0,0 +1,343 @@
+//   <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="motherName">
+//       Mothers Name <span style={{ color: "red" }}>*</span>
+//     </label>
+
+//     <input
+//       type="text"
+//       className="form-control"
+//       id="motherName"
+//       placeholder="Enter Mothers Name"
+//       onChange={this.ChangeHandler("motherName")}
+//       // defaultValue={values.motherName}
+//     />
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="spouseName">Spouse Name</label>
+
+//     <input
+//       type="text"
+//       className="form-control"
+//       id="spouseName"
+//       placeholder="Enter Spouse Name"
+//       // onChange={this.ChangeHandler("spouseName")}
+//       // defaultValue={values.spouseName}
+//     />
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="spouseName">
+//       Nationality <span style={{ color: "red" }}>*</span>
+//     </label>
+
+//     <select
+//       className="form-control"
+//       disabled
+//       // onChange={this.ChangeHandler("nationality")}
+//       // defaultValue={values.nationality}
+//     >
+//       <option id="nationality" value="bangladeshi">
+//         BANGLADESHI
+//       </option>
+//     </select>
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="birthPlace">Birth Place</label>
+
+//     <input
+//       type="text"
+//       className="form-control"
+//       id="birthPlace"
+//       placeholder="Enter Birth Place"
+//       // onChange={this.ChangeHandler("birthPlace")}
+//       // defaultValue={values.birthPlace}
+//     />
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="gender">
+//       Gender <span style={{ color: "red" }}>*</span>
+//     </label>
+
+//     <select
+//       className="form-control"
+//       disabled
+//       // onChange={this.ChangeHandler("gender")}
+//       // defaultValue={values.gender}
+//     >
+//       <option id="male" value="male">
+//         Male
+//       </option>
+
+//       <option id="female" value="female">
+//         Female
+//       </option>
+
+//       <option id="third" value="third">
+//         Third
+//       </option>
+//     </select>
+//   </div>
+// </div>
+// {/* <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="preferredProduct">
+//       Preferred Product{" "}
+//       <span style={{ color: "red" }}>*</span>
+//     </label>
+
+//     <select
+//       className="form-control"
+//       disabled
+//       // onChange={this.ChangeHandler("preferredProduct")}
+//       // defaultValue={values.preferredProduct}
+//     >
+//       <option id="saccount" value="saccount">
+//         Saving Account
+//       </option>
+
+//       <option id="caccount" value="caccount">
+//         Current Account
+//       </option>
+
+//       <option id="fdraccount" value="fdraccount">
+//         FDR Account
+//       </option>
+//     </select>
+//   </div>
+// </div> */}
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="districtId">Presently Resided City</label>
+
+//     <select
+//       className="form-control"
+//       // onChange={this.ChangeHandler("districtId")}
+//       // defaultValue={values.districtId}
+//     >
+//       <option id="saccount" value="saccount">
+//         Resident
+//       </option>
+
+//       <option id="ctg" value="ctg">
+//         Chattogram City Corporation
+//       </option>
+
+//       <option id="dhk" value="dhk">
+//         Dhaka City Corporation
+//       </option>
+
+//       <option id="khl" value="khl">
+//         Khulna City Corporation
+//       </option>
+
+//       <option id="nar" value="nar">
+//         Narayangonj City Corporation
+//       </option>
+
+//       <option id="raj" value="raj">
+//         Rajshahi City Corporation
+//       </option>
+
+//       <option id="syl" value="syl">
+//         Sylhet City Corporation
+//       </option>
+//     </select>
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="branchId">Preferred Branch</label>
+
+//     <select
+//       className="form-control"
+//       // onChange={this.ChangeHandler("branchId")}
+//       // defaultValue={values.branchId}
+//     >
+//       <option id="gulshan" value="gulshan">
+//         Branch A
+//       </option>
+//       <option id="gulshan" value="gulshan">
+//         Branch B
+//       </option>
+//       <option id="gulshan" value="gulshan">
+//         Branch C
+//       </option>
+//     </select>
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="residentStatus">Resident Status</label>
+
+//     <select
+//       className="form-control"
+//       // onChange={this.ChangeHandler("residentStatus")}
+//       // defaultValue={values.residentStatus}
+//     >
+//       <option id="resident" value="resident">
+//         Resident
+//       </option>
+
+//       <option id="nonresident" value="nonresident">
+//         Non-Resident
+//       </option>
+//     </select>
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="residentialStatus">
+//       Residential Status
+//     </label>
+
+//     <select
+//       className="form-control"
+//       // onChange={this.ChangeHandler("residentialStatus")}
+//       // defaultValue={values.residentialStatus}
+//     >
+//       <option id="none" value="">
+//         select
+//       </option>
+//       <option id="own" value="own">
+//         Own
+//       </option>
+//       <option id="rented" value="rented">
+//         Rented
+//       </option>
+//       <option id="others" value="others">
+//         Others
+//       </option>
+//     </select>
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="occupation">Occupation</label>
+
+//     <select
+//       className="form-control"
+//       // onChange={this.ChangeHandler("occupation")}
+//       // defaultValue={values.occupation}
+//     >
+//       <option value="">select</option>
+//       <option value="service">Service</option>
+//       <option value="business">Business</option>
+//       <option value="others">Others</option>
+//     </select>
+//   </div>
+// </div>
+// <div className="col-md-4 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="maritalStatus">
+//       Marital Status <span style={{ color: "red" }}>*</span>
+//     </label>
+
+//     <select
+//       className="form-control"
+//       // onChange={this.ChangeHandler("maritalStatus")}
+//       // defaultValue={values.maritalStatus}
+//     >
+//       <option id="none" value="">
+//         select
+//       </option>
+//       <option id="married" value="married">
+//         Married
+//       </option>
+//       <option id="singale" value="singale">
+//         Singale
+//       </option>
+//     </select>
+//   </div>
+// </div>
+// <div className="col-md-6 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="presentAddress">
+//       Present Address <span style={{ color: "red" }}>*</span>
+//     </label>
+//     <textarea
+//       type="text"
+//       rows="3"
+//       id="permanentAddress"
+//       className="form-control"
+//       // onChange={this.ChangeHandler("presentAddress")}
+//       // defaultValue={values.presentAddress}
+//       placeholder="Enter Present Address"
+//     ></textarea>
+//   </div>
+// </div>
+// <div className="col-md-6 d-inline-block">
+//   <div className="form-group">
+//     <label htmlFor="permanentAddress">
+//       Permanent Address{" "}
+//       <span style={{ color: "red" }}>*</span>
+//     </label>
+//     <textarea
+//       type="text"
+//       rows="3"
+//       id="permanentAddress"
+//       className="form-control"
+//       // onChange={this.ChangeHandler("permanentAddress")}
+//       // defaultValue={values.permanentAddress}
+//       placeholder="Enter Permanent Address"
+//     ></textarea>
+//   </div>
+// </div>
+
+export let listofFirst = [
+  {
+    dim: "6",
+    id: "identifierNumber",
+    title: "Identifier Number",
+    isMandatory: true,
+    placeholder: "Enter Identifier Number",
+    disable: true,
+  },
+  {
+    dim: "6",
+    id: "fullNameEn",
+    title: "fullName",
+    isMandatory: true,
+    placeholder: "Enter full Name",
+    disable: true,
+  },
+  {
+    dim: "6",
+    id: "dob",
+    title: "Date of birth",
+    isMandatory: true,
+    placeholder: "Enter date of birth",
+    disable: true,
+  },
+  {
+    dim: "6",
+    id: "fullNameBn",
+    title: "পূর্ণনাম",
+    isMandatory: true,
+    placeholder: "পূর্ণনাম",
+    disable: true,
+  },
+  {
+    dim: "6",
+    id: "mobile",
+    title: "Mobile Number",
+    isMandatory: true,
+    placeholder: "Mobile Number",
+    disable: true,
+  },
+  {
+    dim: "6",
+    id: "email",
+    title: "E-mail",
+    isMandatory: false,
+    placeholder: "Enter E-mail",
+    disable: false,
+  },
+];
