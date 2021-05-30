c1b38777faf65bdd33d8cb4058ac40df5787f4bc
Moin <moin@commlinkinfotech.com>
2021-05-30 15:38:50 +0600

    commit latest

--- src/app/UserApplication/AccountList.js
+++ src/app/UserApplication/AccountList.js
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
 import MUIDataTable from "mui-datatables";
 import { Link } from "react-router-dom";
+import { instance, baseURL } from "../service/ApiUrls";
 
+let xx = [];
 export class AccountList extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  flattenObject = (ob) => {
+    const toReturn = {};
+
+    Object.keys(ob).map((i) => {
+      if (typeof ob[i] === "object" && ob[i] !== null) {
+        const flatObject = this.flattenObject(ob[i]);
+        Object.keys(flatObject).map((x) => {
+          toReturn[`${i}.${x}`] = flatObject[x];
+          return x;
+        });
+      } else {
+        toReturn[i] = ob[i];
+      }
+      return i;
+    });
+    return toReturn;
+  };
+
+  componentDidMount() {
+    instance.post(baseURL + "/getAllAccounts").then((res) => {
+      if (res.data.result.error === false) {
+        this.setState({ content: res.data.data }, () => {
+          xx = [];
+          this.state.content.map((v) => {
+            xx.push(this.flattenObject(v));
+          });
+          this.setState({ converted: xx });
+        });
+      }
+    });
   }
 
   render() {
@@ -17,62 +51,6 @@ export class AccountList extends Component {
           filter: true,
           sort: true,
         },
-      },
-      {
-        name: "accountName",
-        label: "Account Name",
-        options: {
-          filter: true,
-          sort: false,
-        },
-      },
-      {
-        name: "accountType",
-        label: "Account Type",
-        options: {
-          filter: true,
-          sort: false,
-        },
-      },
-      {
-        name: "status",
-        label: "Status",
-        options: {
-          filter: true,
-          sort: false,
-        },
-      },
-      {
-        name: "Action",
-        options: {
-          filter: false,
-          sort: false,
-          empty: true,
-          customBodyRenderLite: (dataIndex) => {
-            return <Link to="">View</Link>;
-          },
-        },
-      },
-    ];
-
-    const data = [
-      {
-        accountNo: "12012340",
-        accountName: "Moin Mostakin",
-        accountType: "Single",
-        status: "Active",
-      },
-      {
-        accountNo: "12012456",
-        accountName: "Tarik Mahamud",
-        accountType: "Single",
-        status: "Pending",
-      },
-      {
-        accountNo: "12355146",
-        accountName: "Mohammad Abu Taleb",
-        accountType: "Joint",
-        status: "Active",
       },
     ];
 
@@ -91,7 +69,7 @@ export class AccountList extends Component {
                   <div className="col-md-12">
                     <MUIDataTable
                       title={"Account List"}
-                      data={data}
+                      data={this.state.converted}
                       columns={columns}
                       options={options}
                     />
