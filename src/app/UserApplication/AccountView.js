941c46eadbd069b4fdf06ea9e406e0c18e8e2df6
taleb <taleb@commlinkinfotech.com>
2021-05-31 03:27:49 +0600

    Account Views

--- src/app/UserApplication/AccountView.js
+++ src/app/UserApplication/AccountView.js
@@ -1,12 +1,178 @@
 import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import TextBox from "../components/TextBox";
+import {
+  account1,
+  account2,
+  branch,
+  account3,
+  nominee,
+} from "../components/accounts";
+import "react-tabs/style/react-tabs.css";
 
 export default class AccountView extends Component {
   constructor(props) {
     super(props);
-    this.state = { ...props.location.state };
+    this.state = { ...props.location.state.datToload };
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="row align-items-start proBanner mt-4">
+          <div className="col-md-12 grid-margin">
+            <div className="card">
+              <div className="card-body">
+                <Tabs>
+                  <TabList>
+                    <Tab>Account Info</Tab>
+                    <Tab>Nominee</Tab>
+                    <Tab>Transaction Profile</Tab>
+                    <Tab>Documents</Tab>
+                  </TabList>
+
+                  <TabPanel>
+                    <div
+                      className="row justify-content-md-start mb-2 mt-4 p-3"
+                      id="submit1"
+                    >
+                      {account1.map((v, k) => {
+                        //console.log(v, k);
+                        {
+                          return (
+                            <TextBox
+                              dim={v.dim}
+                              id={v.id}
+                              title={v.title}
+                              isMandatory={v.isMandatory}
+                              disable={v.disable}
+                              val={
+                                this.state.product[v.id] !== undefined &&
+                                this.state.product[v.id] !== null
+                                  ? this.state.product[v.id]
+                                  : "N/A"
+                              }
+                            />
+                          );
+                        }
+                      })}
+                      {account2.map((v, k) => {
+                        //console.log(v, k);
+                        {
+                          return (
+                            <TextBox
+                              dim={v.dim}
+                              id={v.id}
+                              title={v.title}
+                              isMandatory={v.isMandatory}
+                              disable={v.disable}
+                              val={
+                                this.state.account[v.id] === false
+                                  ? "Single"
+                                  : "Joint"
+                              }
+                            />
+                          );
+                        }
+                      })}
+                      {branch.map((v, k) => {
+                        //console.log(v, k);
+                        {
+                          return (
+                            <TextBox
+                              dim={v.dim}
+                              id={v.id}
+                              title={v.title}
+                              isMandatory={v.isMandatory}
+                              disable={v.disable}
+                              val={
+                                this.state.branch[v.id] !== undefined &&
+                                this.state.branch[v.id] !== null
+                                  ? this.state.branch[v.id]
+                                  : "N/A"
+                              }
+                            />
+                          );
+                        }
+                      })}
+                      {account3.map((v, k) => {
+                        //console.log(v, k);
+                        {
+                          return (
+                            <TextBox
+                              dim={v.dim}
+                              id={v.id}
+                              title={v.title}
+                              isMandatory={v.isMandatory}
+                              disable={v.disable}
+                              val={
+                                this.state.account[v.id] !== undefined &&
+                                this.state.account[v.id] !== null
+                                  ? this.state.account[v.id]
+                                  : "N/A"
+                              }
+                            />
+                          );
+                        }
+                      })}
+                      <div className="col-md-12">
+                        <p className="preview-p">Account Owner</p>
+                        <table className="table table-striped table-bordered">
+                          <thead>
+                            <tr>
+                              <th>Name</th>
+                              <th>Date of Birth</th>
+                              <th>Marital Status</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {this.state.listCustomers.map((owner) => (
+                              <tr>
+                                <td>{owner.cp.name}</td>
+                                <td>{owner.cp.dob}</td>
+                                <td>{owner.cp.marital_status}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </TabPanel>
+                  <TabPanel>
+                    <div
+                      className="row justify-content-md-start mb-2 mt-4 p-3"
+                      id="submit1"
+                    >
+                      {nominee.map((v, k) => {
+                        //console.log(v, k);
+                        {
+                          return (
+                            <TextBox
+                              dim={v.dim}
+                              id={v.id}
+                              title={v.title}
+                              isMandatory={v.isMandatory}
+                              disable={v.disable}
+                              val={
+                                this.state.nomineeInfo[v.id] !== undefined &&
+                                this.state.nomineeInfo[v.id] !== null
+                                  ? this.state.nomineeInfo[v.id]
+                                  : "N/A"
+                              }
+                            />
+                          );
+                        }
+                      })}
+                    </div>
+                  </TabPanel>
+                  <TabPanel>Transaction Profile</TabPanel>
+                  <TabPanel>Documents</TabPanel>
+                </Tabs>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
---
+++ src/app/components/accounts.js
@@ -0,0 +1,117 @@
+export let account1 = [
+  {
+    dim: "3",
+    id: "name",
+    title: "Product Type",
+    isMandatory: true,
+    val: "Saving",
+    disable: true,
+  },
+];
+export let account2 = [
+  {
+    dim: "3",
+    id: "accountType",
+    title: "Account Type",
+    isMandatory: true,
+    val: "Single",
+    disable: true,
+  },
+];
+export let account3 = [
+  {
+    dim: "3",
+    id: "initialDeposit",
+    title: "Initial Deposit",
+    isMandatory: true,
+    val: "2000",
+    disable: true,
+  },
+];
+export let branch = [
+  {
+    dim: "3",
+    id: "name",
+    title: "Preferred Branch",
+    isMandatory: true,
+    val: "Gulshan",
+    disable: true,
+  },
+];
+export let nominee = [
+  {
+    dim: "3",
+    id: "name",
+    title: "Nominee Name",
+    isMandatory: true,
+    val: "",
+    disable: false,
+  },
+  {
+    dim: "3",
+    id: "dob",
+    title: "Date of Birth",
+    isMandatory: true,
+    val: "",
+    disable: false,
+  },
+  {
+    dim: "3",
+    id: "relation",
+    title: "Relation with A/C Holder",
+    isMandatory: false,
+    val: "",
+    disable: false,
+  },
+  {
+    dim: "3",
+    id: "sharePercent",
+    title: "Share Percentage",
+    isMandatory: false,
+    val: "",
+    disable: false,
+  },
+  {
+    dim: "3",
+    id: "identityType",
+    title: "Identification Type",
+    isMandatory: false,
+    val: "",
+    isMandatory: true,
+    disable: false,
+  },
+  {
+    dim: "3",
+    id: "identityNumber",
+    title: "Identification Number",
+    isMandatory: true,
+    val: "",
+    disable: false,
+  },
+];
+export let newAccount = [
+  {
+    dim: "6",
+    id: "accountType",
+    title: "Account Type",
+    isMandatory: true,
+    val: "Single",
+    disable: true,
+  },
+  {
+    dim: "6",
+    id: "mtransectionlimit",
+    title: "Monthly Transection Amount",
+    isMandatory: true,
+    val: "Up to 100,000BDT",
+    disable: true,
+  },
+  {
+    dim: "6",
+    id: "preferred",
+    title: "Preferred Branch",
+    isMandatory: true,
+    val: "Gulshan",
+    disable: true,
+  },
+];
