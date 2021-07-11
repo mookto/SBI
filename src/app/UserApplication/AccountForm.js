7a82b999ccf7e7d5d7f8c98df6dfe2b8b1f39e96
Moin <moin@commlinkinfotech.com>
2021-07-11 12:14:14 +0600

    commit latest

--- src/app/UserApplication/AccountForm.js
+++ src/app/UserApplication/AccountForm.js
@@ -194,13 +194,15 @@ class AccountForm extends Component {
 
     //console.log(JSON.stringify(props.location.state));
     this.state = {
-      ...props.location.state,
-      //...json,
+      //...props.location.state,
+      ...json,
       hishabprokriti: "মুদারাবাহ্ সঞ্চয়ী হিসাব ",
       mudra: "টাকা",
       hishabporichalona: "এককভাবে",
-      debitCard: props.location.state.datToload.account.debitCard,
-      smsAlert: props.location.state.datToload.account.smsAlert,
+      // debitCard: props.location.state.datToload.account.debitCard,
+      // smsAlert: props.location.state.datToload.account.smsAlert,
+      debitCard: json.datToload.account.debitCard,
+      smsAlert: json.datToload.account.smsAlert,
       internetbanking: true,
       onnano: false,
       lingo: "পুরুষ",
