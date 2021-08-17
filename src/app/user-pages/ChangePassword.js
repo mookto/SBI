fb3d1625127b45294b65b867c5d948561bb3b8b0
Abu Taleb <abutaleb142@gmail.com>
2021-08-18 02:51:54 +0600

    ForgetPassword.js and ChangePassword.js

---
+++ src/app/user-pages/ChangePassword.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import { confirmAlert } from "react-confirm-alert";
+import Loader from "../components/Loader";
+
+class ChangePassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaderShow: false };
+  }
+
+  handleOnChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="row justify-content-center mt-3">
+          <div
+            className="col-3"
+            style={{
+              borderRight: "1px dotted gray",
+              marginRight: "10px",
+            }}
+          >
+            <div className="page-header">
+              <h3 className="page-title"> Change Password</h3>
+            </div>
+            <p style={{}}>
+              {" "}
+              In order to <b>protect your account,</b> make sure your password:
+            </p>
+            <p>
+              <i className="fa fa-check mr-1"></i>At least 6 characters
+            </p>
+            <p>
+              <i className="fa fa-check mr-1"></i>At least 1 upper case letter
+              (A-Z){" "}
+            </p>
+            <p>
+              <i className="fa fa-check mr-1"></i>At least 1 lower case letter
+              (a-z){" "}
+            </p>
+            <p>
+              <i className="fa fa-check mr-1"></i>At least 1 number (0-9)
+            </p>
+            {/* <p>
+              <i className="fa fa-check mr-1"></i>Don't use last used password
+            </p> */}
+            <p>
+              <i className="fa fa-check mr-1"></i>Don't use your username or
+              email
+            </p>
+          </div>
+          <div className="col-4">
+            <form className="pt-2">
+              <p style={{ fontWeight: "bold" }}>
+                Please fillup below Information:
+              </p>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  id="oldPassword"
+                  name="currentPassword"
+                  onChange={this.handleOnChange}
+                  placeholder="Enter Old Password"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  id="newPassword"
+                  name="newPasswordSt"
+                  onChange={this.handleOnChange}
+                  placeholder="Enter New Password"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  id="confirmPassword"
+                  name="confirmedPasswordSt"
+                  onChange={this.handleOnChange}
+                  placeholder="Enter Confirm Password"
+                />
+              </div>
+              <div className="mt-3">
+                <button
+                  className="btn btn-block btn-success btn-lg font-weight-medium auth-form-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    console.log(this.state);
+                  }}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <Loader
+          loaderShow={this.state.loaderShow}
+          onHide={this.loaderHideHandler}
+          loaderHideHandler={this.loaderHideHandler}
+        />
+      </div>
+    );
+  }
+}
+
+export default ChangePassword;
---
+++ src/app/user-pages/ForgetPassword.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "../../assets/styles/signup.css";
+import { login } from "../service/ApiService";
+
+export class ForgetPassword extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  ChangeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="container main">
+          <img
+            className="center"
+            src={process.env.PUBLIC_URL + "/gib-logo.jpg"}
+            alt="logo"
+          />
+          <div className="register">
+            <div className="row">
+              <div className="col-md-3 register-left">
+                <img
+                  className="center"
+                  src={process.env.PUBLIC_URL + "/logo-dbs-black.png"}
+                  alt="logo"
+                  width="170px"
+                />
+                <h4 style={{ color: "#000000" }}>Welcome to</h4>
+                <p style={{ color: "#000000" }}>Digital Banking System!</p>
+              </div>
+              <div className="col-md-9 register-right">
+                <div className="tab-content" id="myTabContent">
+                  <h3 className="register-heading">Forget Password</h3>
+                  <div className="row register-form">
+                    <div className="col-md-12" style={{ textAlign: "center" }}>
+                      <div className="row justify-content-md-center">
+                        <div className="form-group col-md-8 mb-0">
+                          <div
+                            className={
+                              this.state.error === true
+                                ? "alert alert-danger fade show"
+                                : "d-none"
+                            }
+                            role="alert"
+                          >
+                            <p
+                              style={{ fontSize: "16px", marginBottom: "0px" }}
+                            >
+                              {this.state.errorMessage}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          // minlength="10"
+                          // maxlength="10"
+                          name="email"
+                          className="form-control"
+                          placeholder="Enter Email Address"
+                          onChange={this.ChangeHandler}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-12" style={{ textAlign: "center" }}>
+                      <div className="form-group">
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          onClick={() => {}}
+                          disabled={this.state.isLoading}
+                        >
+                          Reset Password
+                        </button>
+                      </div>
+                    </div>
+                    <div className="col-md-12" style={{ textAlign: "center" }}>
+                      <div className="form-group">
+                        <button
+                          type="button"
+                          className="btn btn-success"
+                          onClick={() => {}}
+                        >
+                          Sign In
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ForgetPassword;
