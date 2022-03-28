8b8157d566ac2aa067eb470e3252ada651c4509c
Moin <moin@commlinkinfotech.com>
2022-03-29 00:53:11 +0600

    commit latest

--- src/app/service/ApiService.js
+++ src/app/service/ApiService.js
@@ -9,6 +9,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 // export const baseURL =
 //   "https://ekycmiddleware.globalislamibankbd.com:8443/ekyc";
 export const baseURL = "https://ekycrelay.globalislamibankbd.com:8443/ekyc";
+//export const baseURL = "https://ekycweb.globalislamibankbd.com/apiserver";
+// export const baseURL = "https://103.88.137.186:8443/ekyc";
 // const baseURL = "http://180.210.129.103:8080/mdm";
 // const baseURL = "http://localhost:8080"
 const loginURL = "/oauth/token";
